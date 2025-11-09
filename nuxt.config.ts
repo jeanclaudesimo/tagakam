4b686c18c14bd1f75f7e6e103d18e508cd128461
jeanclaudesimo <jeanclaude.simo@abus-kransysteme.de>
2025-11-09 20:35:37 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+
+  nitro: {
+    preset: 'node'
+  },
 
   modules: [
     '@nuxtjs/tailwindcss',
